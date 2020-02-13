bcc86f5d5ccfb23b368a94de1a5bfe9a77580702
jtandy123 <982071036@qq.com>
2020-02-13 23:15:21 +0800

    update event emitter demo

--- nodejsinaction/eventemitter/channel.js
+++ nodejsinaction/eventemitter/channel.js
@@ -5,11 +5,14 @@ channel.clients = {};
 channel.subscriptions = {};
 
 channel.on('join', function(id, client) {
-  console.log(this); // {}
+  console.log(this); // {}，this指向module.exports
+  const welcome = `
+  Welcome!
+  Guests online: ${this.listeners('broadcast').length}
+  `;
+  client.write(`${welcome}\n`);
   this.clients[id] = client;
   this.subscriptions[id] = (senderId, message) => {
-    console.log('id: ', id);
-    console.log('senderId: ', senderId);
     if (id !== senderId) {
       this.clients[id].write(message);
     }
@@ -17,12 +20,29 @@ channel.on('join', function(id, client) {
   this.on('broadcast', this.subscriptions[id]);
 });
 
+channel.on('leave', function(id) {
+  this.removeListener('broadcast', this.subscriptions[id]);
+  this.emit('broadcast', id, `${id} has left the chatroom.\n`);
+});
+
+channel.on('shutdown', () => {
+  channel.emit('broadcast', '', 'The server has shut down.\n');
+  channel.removeAllListeners('broadcast');
+})
+
 const server = net.createServer(client => {
   const id = `${client.remoteAddress}:${client.remotePort}`;
   channel.emit('join', id, client);
   client.on('data', data => {
     data = data.toString();
+    if (data === 'shutdown\r\n') {
+      channel.emit('shutdown');
+    }
     channel.emit('broadcast', id, data);
+  });
+
+  client.on('close', () => {
+    channel.emit('leave', id);
   });
 });
 server.listen(8888);
