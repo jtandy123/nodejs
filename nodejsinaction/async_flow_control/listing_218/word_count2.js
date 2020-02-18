e64a6ad6bb8cb4ea237bfaa03e63ca18962edcf5
jtandy123 <982071036@qq.com>
2020-02-18 10:48:52 +0800

    async flow control

---
+++ nodejsinaction/async_flow_control/listing_218/word_count2.js
@@ -0,0 +1,59 @@
+const fs = require('fs');
+const tasks = [];
+const wordCounts = {};
+const filesDir = './text';
+const limit = 2;
+let running = 0;
+
+function checkIfComplete() {
+  running--;
+  if (tasks.length === 0 && running === 0) {
+    for (let index in wordCounts) {
+      console.log(`${index}: ${wordCounts[index]}`);
+    }
+  }
+}
+
+function addWordCount(word) {
+  wordCounts[word] = (wordCounts[word]) ? wordCounts[word] + 1 : 1;
+}
+
+function countWordsInText(text) {
+  const words = text.toString().toLowerCase().split(/\W+/).sort();
+
+  words.filter(word => word).forEach(word => addWordCount(word));
+}
+
+fs.readdir(filesDir, (err, files) => {
+  if (err) {
+    throw err;
+  }
+
+  function launcher() {
+    while(running < limit && tasks.length > 0) {
+      const task = tasks.shift();
+      task();
+    }
+  }
+
+  files.forEach(file => {
+    const task = (file => {
+      return () => {
+        running++;
+        fs.readFile(file, (err, text) => {
+          if (err) {
+            throw err;
+          }
+          if (tasks.length > 0) {
+            launcher();
+          }
+          countWordsInText(text);
+          checkIfComplete();
+        })
+      };
+    })(`${filesDir}/${file}`);
+    tasks.push(task);
+  });
+
+  launcher();
+});
---
+++ nodejsinaction/async_flow_control/testAsyncModule.js
@@ -0,0 +1,32 @@
+setTimeout(() => {
+  console.log('I execute first.');
+  setTimeout(() => {
+    console.log('I execute next.');
+    setTimeout(() => {
+      console.log('I execute last.')
+    }, 100)
+  }, 500);
+}, 1000);
+
+
+const async = require('async');
+async.series([
+  (callback) => {
+    setTimeout(() => {
+      console.log('I execute first.');
+      callback();
+    }, 1000);
+  },
+  (callback) => {
+    setTimeout(() => {
+      console.log('I execute next.');
+      callback();
+    }, 500);
+  },
+  (callback) => {
+    setTimeout(() => {
+      console.log('I execute last.');
+      callback();
+    }, 100);
+  },
+]);
